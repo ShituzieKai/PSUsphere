007e9dde65382c1cf311a9b5daecdc275207e232
ShituzieKai <mamansagkaiser@gmail.com>
2026-09-09 11:28:36 +0800

    Update create_initial_data with college and program

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 import os
 import socket
 from pathlib import Path
-<<<<<<< HEAD
-=======
-import os
->>>>>>> a4e14e8 (update settings and static for deployment)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
